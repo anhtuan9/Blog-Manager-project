a411a1e0a257cd108d68fabd379ef2ab8ed71272
Kot <kotvnn@msn.com>
2018-12-11 10:55:08 +0700

    Edit and Del blog

--- src/main/java/com/TripleT/BlogManager/controller/BlogController.java
+++ src/main/java/com/TripleT/BlogManager/controller/BlogController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -36,5 +33,27 @@ public class BlogController {
         blogService.save(blog);
         modelAndView.addObject("message", "Add Success !");
         return modelAndView;
+    }
+
+    @GetMapping("/post/{id}")
+    public ModelAndView showEdit(@PathVariable Long id) {
+        Blog blog = blogService.findById(id);
+        ModelAndView modelAndView = new ModelAndView("edit");
+        modelAndView.addObject("blog", blog);
+        return modelAndView;
+    }
+
+    @PostMapping("/post")
+    public ModelAndView editPost(@ModelAttribute("post") Blog blog) {
+        ModelAndView modelAndView = new ModelAndView("edit");
+        modelAndView.addObject("blog", blog);
+        modelAndView.addObject("message", "Edit Success !");
+        return modelAndView;
+    }
+
+    @GetMapping("/del/{id}")
+    public String delStar(@PathVariable Long id) {
+        blogService.remove(id);
+        return "redirect:/admin";
     }
 }
